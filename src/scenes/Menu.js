27e57f36f234bd4a64f2b6564619e3b35456b656
ericshen9999 <dragoneshen@gmail.com>
2020-04-28 15:44:07 -0700

    Added Button

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,51 +10,25 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        /* Load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-        this.load.audio('bg_music', './assets/ArcadeEscapade.mp3'); */
+        //520 x 180 Start Button
+        this.load.image('start','./assets/Start.png');
     }
-
     create(){
-        /* Displaying the menu
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
-        // Show menu text
-        let centerX = game.config.width/2;
-        let centerY = game.config.height/2;
-        let textSpacer = 64;
+        //Button
+        var button = this.add.sprite(190,200,`start`).setScale(0.5,0.5).setOrigin(0,0).setInteractive();
+        button.on('pointerover',this.over);
+        button.on('pointerout',this.out);
+        button.on('pointerup',this.up,this);
 
-        this.add.text(centerX, centerY-textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY, '1 Use ⟷ to move & F to fire', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + textSpacer, '2 Use numpad 4 & 6 to move & + to fire', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + (textSpacer * 2), '0.1 extra seconds for each hit', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(centerX, centerY + (textSpacer * 3), 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0.5);
-        console.log(this);
-
-        
         // Define keyboard keys
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        //keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        //keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
         // Change scenes
         // this.scene.start("playScene");
         
-        game.music = this.sound.add('bg_music');
-        game.music.setLoop(true); */
+        //game.music = this.sound.add('bg_music');
+        //game.music.setLoop(true);
     }
 
     update() { /*
@@ -76,5 +50,19 @@ class Menu extends Phaser.Scene {
             this.sound.play('sfx_select');
             this.scene.start('playScene');
         } */
+    }
+
+    //If mouse clicks button
+    up() {
+        console.log('changed scene')
+        this.scene.start("playScene");
+    }
+    //If mouse hovers button (animation?)
+    over() {
+        console.log('button over');
+    }
+    //If mouse leaves button (animation?)
+    out() {
+        console.log('button out');
     }
 }
