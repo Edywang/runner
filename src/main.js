9e59738076a81f0371a92c4ec4202611f5d51212
Vincent Cheng <viwcheng@ucsc.edu>
2020-05-03 12:19:53 -0700

    added header with names, title, completion date, and creative tilt justifications

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 /* Explanation of points
-Starting Tier: Add your own (copyright-free) background music to the Play scene (10)
-Novice Tier: Display the time remaining (in seconds) on the screen (15)
-Intermediate Tier: Implement a new timing/scoring mechanism that adds time to the clock for successful hits (25)
-S-Rank Tier: Implement a simultaneous two-player mode (50)
-Design choices surrounding the two-player mode: I split the players' movable space in half and dedicated one half to each player.
-I decided that if both players hit the same spaceship at the same time, both would reset and both hits would be counted (Kind of hard to replicate alone)
-The spaceships do not collide with each other beyond being restricted in their own play spaces, I decided this would go against the cooperative nature.
+Team: Eric Shen, Edwin Wang, Vincent Cheng
+Title: Infinite Football
+Completed: 5/3/2020
+Creative tilt: The art (while not great) was mostly drawn ourselves, and for technically interesting, we used a particle emitter for the portal
 */
 
 
